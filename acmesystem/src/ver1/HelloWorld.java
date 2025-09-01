2d3aeb9be1509c48f0a50193c7bfdce09dd450d9
Seth Tucker <sethdanieltucker@gmail.com>
2025-09-01 00:10:25 -0400

    Letting everyone know that Seth is working locally on his branch.

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
+		System.out.println("Seth is working locally on his branch");
 
 	}
 
