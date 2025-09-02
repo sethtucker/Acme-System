24b53972cd8c21b2d68bbd960fab3e38be062941
Seth Tucker <sethdanieltucker@gmail.com>
2025-09-01 21:22:09 -0400

    Added heyNow method to HelloWorld.

--- acmesystem/src/ver1/HelloWorld.java
+++ acmesystem/src/ver1/HelloWorld.java
@@ -7,5 +7,10 @@ public class HelloWorld {
 		System.out.println("Seth is working locally on his branch");
 
 	}
-
+	
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
+	}
+	
 }
